9823392ca104637cb8b0bfdfb914c2a75f897988
Florian Louvet <florian.louvet@epitech.eu>
2019-04-07 02:54:07 +0200

    [+] Added a custom login page

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+$('form').form({
+    fields: {
+        firstName: {
+            identifier: 'email',
+            rules: [
+                {
+                    type: 'email',
+                    prompt: 'Invalid email address'
+                }
+            ]
+        },
+        email: {
+            identifier: 'password',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Invalid password'
+                }
+            ]
+        }
+    },
+    onSuccess: function(e) {
+    }
+});
